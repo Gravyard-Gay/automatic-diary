5330643f0d63cf07c655ff933b9bd0bfa7d312d7
Gustavo <enfianocu15@gmail.comgit config --global user.name Gustavogit config --global user.email enfianocu15@gmail.com>
2022-09-22 15:48:29 -0300

    Styling the GUI

--- app.py
+++ app.py
@@ -2,14 +2,18 @@ from tkinter import *
 import customtkinter as ctk
 import os
 
-
 root = Tk()
 root.geometry("640x640")
 root.title("Auto Diary")
 
+
 #region [ Variables ]
 btnRadius = 20
-
+bgc = "#5a068e"
+txtbg = "#120024"
+txtfg = "#fff"
+btnbg = "#b7138b"
+btnch = "#6D0B53"
 #endregion
 
 def newNote():
@@ -25,25 +29,31 @@ def newNote():
     cfgFile.write(str(entryNum))
     exit()
 
-
 def openEntryDir():
     os.startfile('entrys')
 
+winBg = Canvas(root, width=640, height=640, bg=bgc).place(x=0, y=0)
+
+title = ctk.CTkLabel(root, text="New Entry:", fg_color=bgc, text_font=("sans-serif", 16)).pack(pady=10)
 
 
-entryField = Text(root, width=50, height=20, font=("sans-serif", 16))
-entryField.pack(pady=30)
+entryField = ctk.CTkTextbox(root, fg_color=txtbg, border=0, corner_radius=30,
+                            height=482, width=560, text_font=("sans-serif", 16),
+                            bg_color=bgc)
+entryField.pack()
 
-btnFrame = Frame(root)
+btnFrame = Frame(root, bg=bgc)
 btnFrame.pack()
 
 
-btnSave = ctk.CTkButton(btnFrame, command=newNote, corner_radius=btnRadius, height=50, text="Save Entry")
-btnOpen = ctk.CTkButton(btnFrame, command=openEntryDir, corner_radius=btnRadius, height=50, text="Open Entrys Folder")
+btnSave = ctk.CTkButton(btnFrame, command=newNote, corner_radius=btnRadius, hover_color=btnch,
+                        height=80, text="Save Entry", fg_color=btnbg, text_font=("sans-serif", 16))
+btnOpen = ctk.CTkButton(btnFrame, command=openEntryDir, corner_radius=btnRadius, hover_color=btnch,
+                        height=80, text="Open Folder", fg_color=btnbg, text_font=("sans-serif", 16))
 
 
-btnSave.grid(column=0, row=0, padx=10)
-btnOpen.grid(column=20, row=0, padx=10)
+btnSave.grid(column=0, row=0,  pady=15,padx=10)
+btnOpen.grid(column=20, row=0, pady=15, padx=10)
 
 
 
